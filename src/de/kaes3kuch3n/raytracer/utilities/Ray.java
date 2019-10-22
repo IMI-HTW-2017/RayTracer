06e0503dd89119b48dc072bca458ce10081d1ec8
Luis Hankel <kaes3kuch3n@gmail.com>
2019-10-22 23:51:37 +0200

    Fixed ray hit calculations

--- src/de/kaes3kuch3n/raytracer/utilities/Ray.java
+++ src/de/kaes3kuch3n/raytracer/utilities/Ray.java
@@ -1,6 +1,5 @@
 package de.kaes3kuch3n.raytracer.utilities;
 
-import de.kaes3kuch3n.raytracer.Camera;
 import de.kaes3kuch3n.raytracer.Sphere;
 
 public class Ray {
@@ -20,11 +19,11 @@ public class Ray {
     public double getDistanceFromOrigin(Sphere sphere) {
         Vector3 spherePos = sphere.getPosition();
         double a = Math.pow(direction.x, 2) + Math.pow(direction.y, 2) + Math.pow(direction.z, 2);
-        double b = 2 * (direction.x * spherePos.x + direction.y * spherePos.y + direction.z * spherePos.z
-                - direction.x * origin.x - direction.y *origin.y - direction.z * origin.z);
-        double c = spherePos.x * spherePos.x + spherePos.y * spherePos.y + spherePos.z * spherePos.z
-                - 2 * spherePos.x * origin.x - 2 * spherePos.y * origin.y - 2 * spherePos.z * origin.z
-                + origin.x * origin.x + origin.y * origin.y + origin.z * origin.z
+        double b = 2 * (origin.x * direction.x + origin.y * direction.y + origin.z * direction.z
+                - direction.x * spherePos.x - direction.y * spherePos.y - direction.z * spherePos.z);
+        double c = origin.x * origin.x + origin.y * origin.y + origin.z * origin.z
+                - 2 * origin.x * spherePos.x - 2 * origin.y * spherePos.y - 2 * origin.z * spherePos.z
+                + spherePos.x * spherePos.x + spherePos.y * spherePos.y + spherePos.z * spherePos.z
                 - sphere.getRadius() * sphere.getRadius();
 
         if(a == 0)
