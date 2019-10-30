443e20daef55c4a63e2292a1f33c2d6b3d49cb52
davidpgl <panagiotopulos.dtt@gmail.com>
2019-10-30 16:26:28 +0100

    [Added Matrix4-Class] [Added Quadric-Class (WIP)]

---
+++ src/de/kaes3kuch3n/raytracer/objects/Quadric.java
@@ -0,0 +1,10 @@
+package de.kaes3kuch3n.raytracer.objects;
+
+import de.kaes3kuch3n.raytracer.utilities.Ray;
+
+public class Quadric {
+
+    public Ray.Hit getRayhit() {
+        return null;
+    }
+}
